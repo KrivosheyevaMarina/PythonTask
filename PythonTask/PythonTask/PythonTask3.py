705eafa7e71b5d09f75544f06aee61946d1d813f
Krivosheyeva Marina <krivosheyevamarina@gmail.com>
2016-07-05 13:57:07 +0300

    Task3.2

--- PythonTask/PythonTask/PythonTask3.py
+++ PythonTask/PythonTask/PythonTask3.py
@@ -1,9 +1,11 @@
+#Task 3
+
 import sys , random
-def compute ( n ):
-    i = 0; s = 0
-    while i <= n:
+def compute (n):
+    i = s = 0
+    while (i <= n):
         s += random . random ()
         i += 1
-    return s /n
-n = sys . argv [1]
-print ('average of %d random numbers is %g ' % (n , compute (n )))
+    return s / n
+n = int(sys . argv [1], 10)
+print ('average of %d random numbers is %g ' % (n, compute(n)))
