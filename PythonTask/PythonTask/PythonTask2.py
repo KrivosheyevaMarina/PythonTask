6247177de635d2a38433e8537994dc35c9fe8250
Krivosheyeva Marina <krivosheyevamarina@gmail.com>
2016-07-05 13:17:23 +0300

    Task 2 Korrect

---
+++ PythonTask/PythonTask/PythonTask2.py
@@ -0,0 +1,13 @@
+#Task 2
+
+import sys, random 
+n=int(sys.argv[1],10) 
+i=0 
+sum=0.0 
+while i<n: 
+    x=random.uniform(-1,1) 
+    sum=sum+a 
+    print ("%.4f" % (x)) 
+    i=i+1 
+middle=sum/n 
+print("middle = ","%.4f" % (middle))
