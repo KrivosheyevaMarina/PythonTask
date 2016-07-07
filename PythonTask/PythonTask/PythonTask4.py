f7ab0332ba313c4fcc278c8f29da1c5734e4fbb7
Krivosheyeva Marina <krivosheyevamarina@gmail.com>
2016-07-07 14:47:43 +0300

    Task4(2)

--- PythonTask/PythonTask/PythonTask4.py
+++ PythonTask/PythonTask/PythonTask4.py
@@ -1,9 +1,22 @@
 #Task 4
       
+#import math,sys
+#for r in sys.argv[1:]:
+#    try:
+#        x = float(r)
+#        if x > 0:
+#            y = math.log(x)
+#            print ( 'ln ( %f ) = %g' % (x,y) )
+#        else: 
+#            print ( 'ln ( %f ) is illegal' % (x) )
+#    except : 
+#        print ("only IaN logarifm we can calculate")
 import math,sys
-for r in sys.argv[1:]:
+x=len(sys.argv)
+rangelist=range(1,x,1)
+for i in rangelist:
     try:
-        x = float(r)
+        x = float(sys.argv[i])
         if x > 0:
             y = math.log(x)
             print ( 'ln ( %f ) = %g' % (x,y) )
